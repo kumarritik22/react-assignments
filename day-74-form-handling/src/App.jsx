b62311d3b5dccd18225d07584bca5713dc8a3017
Ritik Kumar <ritik.kumar22@outlook.com>
2025-12-27 17:16:06 +0530

    Day-74 Assignment added

---
+++ day-74-form-handling/src/App.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react'
+
+function App() {
+
+  const [profilePic, setProfilePic] = useState('')
+  const [name, setName] = useState('')
+  const [occupation, setOccupation] = useState('')
+  const [email, setEmail] = useState('')
+  const [description, setDescription] = useState('')
+  
+  const [allUsers, setAllUsers] = useState([])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    setAllUsers([...allUsers, { profilePic, name, occupation, email, description }])
+
+    setProfilePic ('')
+    setName ('')
+    setOccupation ('')
+    setEmail ('')
+    setDescription ('')
+  }
+
+  return (
+    <div className='bg-slate-900 p-5 min-h-screen'>
+        <div className="flex flex-col items-center gap-6 text-white bg-[]">
+          <h1 className="text-4xl font-semibold text-slate-100">Registration Form</h1>
+          <form className='flex flex-col bg-slate-800 border border-slate-700 rounded-xl shadow-lg transition-all duration-200 p-5' onSubmit={(e) => {
+            submitHandler(e);
+          } }>
+            
+              <input
+                type="url"
+                placeholder='Profile pic'
+                
+                required
+                value={profilePic}
+                onChange={(e)=> {
+                  setProfilePic(e.target.value)
+                }}  
+              />
+            
+                <input type="text"
+                placeholder='Enter Your Name'
+                required
+                value={name}
+                onChange={(e)=> {
+                  setName(e.target.value)
+                }}  
+                />
+              
+              <input type="text"
+                placeholder='Occupation'
+                required
+                value={occupation}
+                onChange={(e)=> {
+                  setOccupation(e.target.value)
+                }}  
+              />
+            
+              <input type="email"
+                placeholder='Enter Your email'
+                required
+                value={email}
+                onChange={(e)=> {
+                  setEmail(e.target.value)
+                }}  
+              />
+              
+              <input type="text"
+                placeholder='Description'
+                required
+                value={description}
+                onChange={(e)=> {
+                  setDescription(e.target.value)
+                }}  
+              />
+
+            <button className="mt-4 self-center bg-sky-500 hover:bg-sky-600 text-white rounded-lg shadow-lg transition-all duration-200 px-4 py-1 border border-[#334155] font-medium">Submit</button>
+          </form>
+        </div>
+
+        {allUsers.map((elem, idx) => {
+          return <div key={idx} className='max-w-75 p-5 rounded-[10px] border border-[#555] mt-10 text-[aliceblue] ml-5 inline-block'>
+                <div className='h-13 w-13 rounded-full overflow-hidden '>
+                  <img src={elem.profilePic} className='h-full w-full object-cover' />
+                </div>  
+                <h1 className='mt-3.5 text-[20px] font-semibold '>{elem.name}</h1>
+                <h3 className='mt-0.75 text-[15px] font-semibold '>{elem.occupation}</h3>
+                <h4 className='mt-0.75 text-[12px] font-semibold '>{elem.email}</h4>
+                <p className='mt-2.5 text-[10px]'>{elem.description}</p>
+            </div>
+        })}
+      </div>
+  )
+}
+
+export default App
